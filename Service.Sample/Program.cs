c02cdad13aa6929c89549cab558306bc5b7f5f74
Dejan Stojanovic <d.stojanovic@hotmail.com>
2018-05-29 22:32:09 +0400

    enabled dockering

--- Service.Sample/Program.cs
+++ Service.Sample/Program.cs
@@ -4,17 +4,18 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Service.Sample
 {
     class Program
     {
-        static CancellationTokenSource tokenSource = new CancellationTokenSource();
+        static readonly CancellationTokenSource tokenSource = new CancellationTokenSource();
+        static readonly String outFolderPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "output");
+        static readonly String outFilePath = Path.Combine(outFolderPath, $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt");
+
         static void Main(string[] args)
         {
-            String outFolderPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "output");
-            String outFilePath = Path.Combine(outFolderPath, $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt");
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
             if(!Directory.Exists(outFolderPath))
             {
@@ -23,7 +24,7 @@ namespace Service.Sample
 
             Console.WriteLine($"Output file: {outFilePath}");
 
-            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE START ------------------ - " }, Encoding.UTF8);
+            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE START ------------------- " }, Encoding.UTF8);
 
             while (!tokenSource.Token.IsCancellationRequested)
             {
@@ -31,11 +32,18 @@ namespace Service.Sample
 
                 Console.WriteLine(DateTime.Now);
 
-                Thread.Sleep(1000);
+                Thread.Sleep(5000);
             }
 
-            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE STOP ------------------ - " }, Encoding.UTF8);
+            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE STOP ------------------- " }, Encoding.UTF8);
         }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            File.AppendAllLines(outFilePath, new List<String>() { "------------------- PROC EXIT SIGNAL ------------------- " }, Encoding.UTF8);
+            tokenSource.Cancel();
+        }
+
 
     }
 }
