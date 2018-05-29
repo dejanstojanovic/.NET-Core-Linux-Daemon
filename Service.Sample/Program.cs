eda3f2587cb30cb47438b55df21ccc3944ef5316
Dejan Stojanovic <d.stojanovic@hotmail.com>
2018-05-29 19:53:12 +0400

    Initial commit

---
+++ Service.Sample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service.Sample
+{
+    class Program
+    {
+        static CancellationTokenSource tokenSource = new CancellationTokenSource();
+        static void Main(string[] args)
+        {
+            String outFolderPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "output");
+            String outFilePath = Path.Combine(outFolderPath, $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt");
+
+            if(!Directory.Exists(outFolderPath))
+            {
+                Directory.CreateDirectory(outFolderPath);
+            }
+
+            Console.WriteLine($"Output file: {outFilePath}");
+
+            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE START ------------------ - " }, Encoding.UTF8);
+
+            while (!tokenSource.Token.IsCancellationRequested)
+            {
+                File.AppendAllLines(outFilePath, new List<String>() { DateTime.Now.ToString() }, Encoding.UTF8);
+
+                Console.WriteLine(DateTime.Now);
+
+                Thread.Sleep(1000);
+            }
+
+            File.AppendAllLines(outFilePath, new List<String>() { "------------------- SERVICE STOP ------------------ - " }, Encoding.UTF8);
+        }
+
+    }
+}
